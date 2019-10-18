803e5cb2a8d97282cb18435666e8378c3498bc93
bagstel <stelios@baglaridis.com>
2019-10-18 17:42:13 +0300

    header position fixed: executing script on page load added

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,12 +1,15 @@
 (function (header) {
-    window.addEventListener('scroll', event => {
-        const vScroll = window.pageYOffset;
-        const scrolledValue = 21;
+  function headerFixed() {
+    const vScroll = window.pageYOffset;
+    const scrolledValue = 21;
 
-        if (vScroll >= scrolledValue) {
-            header.classList.add('header--fixed')
-        } else if (vScroll <= 21) {
-            header.classList.remove('header--fixed')
-        }
-    });
+    if (vScroll >= scrolledValue) {
+      header.classList.add('header--fixed');
+    } else if (vScroll <= 21) {
+      header.classList.remove('header--fixed');
+    }
+  }
+
+  headerFixed();
+  window.addEventListener('scroll', headerFixed);
 })(document.querySelector('.header'));
