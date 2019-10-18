2c938086140cf435905dfa79668ec881aa9202a7
bagstel <stelios@baglaridis.com>
2019-10-19 02:55:35 +0300

    section works changed to vue components and make slider work

---
+++ src/components/tags/tags.js
@@ -0,0 +1,16 @@
+import EventBus from '../../scripts/eventBus';
+
+export default {
+  template: '#tags-template',
+  data () {
+    return {
+      techs: []
+    }
+  },
+  mounted() {
+    EventBus.$on('work', ({ techs }) => {
+      this.techs = techs;
+      console.log(techs);
+    });
+  }
+}
---
+++ src/components/thumbnails/thumbnails.js
@@ -0,0 +1,17 @@
+import EventBus from '../../scripts/eventBus'
+
+export default {
+  template: '#thumbnails',
+  props: {
+    works: Array,
+    activeWork: Number
+  },
+  methods: {
+    changeSlide() {
+      const { index } = event.target.dataset;
+
+      EventBus.$emit('changeSlide', index);
+      console.log(event.target.dataset.index);
+    }
+  }
+}
