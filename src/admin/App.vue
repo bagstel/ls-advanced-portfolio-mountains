75e9f6f096bbacd3ffd472eb8c65c20ff1267afb
bagstel <stelios@baglaridis.com>
2019-10-26 19:20:42 +0300

    admin about done

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,25 +1,2 @@
-<template lang="pug">
-  include ../blocks/form-elements.pug
-  include ../blocks/button.pug
-  .root-wrapper-container
-    include ../blocks/login.pug
-    include ../blocks/header-admin.pug
-    include ../blocks/tabs.pug
-
-</template>
-
-<script>
-
-</script>
-
-
-<style lang="postcss">
-  @import "normalize.css";
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800');
-  @import "../styles/mixins.pcss";
-  @import "../styles/layout/base.pcss";
-  @import "../styles/blocks/button.pcss";
-  @import "../styles/blocks/form-elements.pcss";
-  @import "./styles/*.pcss";
-
-</style>
+<template lang="pug" src="./app.pug"></template>
+<style lang="postcss" src="./app.pcss"></style>
