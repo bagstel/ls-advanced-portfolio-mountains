ff3a74ca32468072f95ba5ed260498d3557ab58d
bagstel <stelios@baglaridis.com>
2019-10-26 11:32:42 +0300

    form-element's pug & pcss modified: split input & textarea into separate mixins from block

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -4,15 +4,8 @@
   .root-wrapper-container
     include ../blocks/login.pug
     include ../blocks/header-admin.pug
-    .tabs
-      .container.tabs__container
-        ul.tabs__list
-          li.tabs__item
-            button(type="button").btn.tabs__btn Обо мне
-          li.tabs__item
-            button(type="button").btn.tabs__btn Работы
-          li.tabs__item
-            button(type="button").btn.tabs__btn Отзывы
+    include ../blocks/tabs.pug
+
 </template>
 
 <script>
@@ -27,35 +20,6 @@
   @import "../styles/layout/base.pcss";
   @import "../styles/blocks/button.pcss";
   @import "../styles/blocks/form-elements.pcss";
-  @import "../styles/blocks/login.pcss";
-  @import "../styles/blocks/header-admin.pcss";
-  @import "../styles/blocks/user.pcss";
-
-  .tabs {
-    &__list {
-      display: flex;
-      height: 77px;
-    }
-
-    &__item {
-      margin-right: 30px;
-      height: 100%;
-    }
-
-    &__btn {
-      width: 125px;
-      color: #414c63;
-      line-height: 2;
-      height: 100%;
-      border-bottom: 3px solid transparent;
-
-      &:hover {
-        color: $links-active;
-        font-weight: 600;
-        border-color: currentColor;
-      }
-    }
-  }
-
+  @import "./styles/*.pcss";
 
 </style>
