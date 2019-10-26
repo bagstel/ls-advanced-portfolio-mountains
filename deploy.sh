da2dc85678bc12f14c902cd1b1c4587049f82a21
bagstel <stelios@baglaridis.com>
2019-10-26 08:18:56 +0300

    postcss-each plugin added and deploy.sh for auto deploy to gh-pages

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+## abort on errors
+set -e
+
+## build
+npm run build
+
+## navigate into the build output directory
+cd dist
+
+## if you are deploying to a custom domain
+echo 'ls-advanced-portfolio-mountains.psd-to.site' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+## if you are deploying to https://<USERNAME>.github.io
+# git add remote origin https://github.com/<USERNAME>/<REPO>.git
+# git push -f origin master
+
+## if you are deploying to https://<USERNAME>.github.io/<REPO>
+git add remote origin https://github.com/bagstel/ls-advanced-portfolio-mountains.git
+git push -f origin master:gh-pages
+
+cd -
+
+## https://cli.vuejs.org/guide/deployment.html#github-pages
